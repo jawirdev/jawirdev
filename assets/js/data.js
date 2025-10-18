ef1b0e10229c18455bd7cab8751b06b6b58f8bdd
jawirdev <budirakka@gmail.com>
2025-10-18 11:04:40 +0700

    Create data.js

---
+++ assets/js/data.js
@@ -0,0 +1,10 @@
+window.DATAFEATURES = [
+  {
+    kategori: 'Downloader',
+    nama: 'Spotify Downloader',
+    deskripsi: 'Downloader lagu via API',
+    file_case: 'codes/downloader/spotify_case.js',
+    file_cjs: 'codes/downloader/spotify_cjs.js',
+    file_esm: 'codes/downloader/spotify_esm.js'
+  }
+];
